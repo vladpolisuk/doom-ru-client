8ea3e66be2c9cd79c265e2a9fe7ecfa1a909390e
Vladislav Polishchuk <vlad@polisuk.ru>
2023-02-18 23:13:08 +0300

    fix

--- pages/legal/license-agreement.tsx
+++ pages/legal/license-agreement.tsx
@@ -9,11 +9,11 @@ const DynamicPage = dynamic(() => import('../../modules/PageLegal/LicenseAgreeme
 
 export default function LicenseAgreement() {
 	const t = useTranslation('common').t;
-	const t_home = useTranslation('home').t;
+	const t_legal = useTranslation('legal').t;
 
 	const description = t('site_description');
 	const keywords = t('site_keywords');
-	const title = t_home('home_title');
+	const title = t_legal('legal_licenseAgreement_title');
 	const author = t('site_author');
 
 	return (
@@ -42,7 +42,7 @@ export default function LicenseAgreement() {
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
 	return {
 		props: {
-			...(await serverSideTranslations(locale as string, ['common', 'header', 'footer']))
+			...(await serverSideTranslations(locale as string, ['common', 'header', 'legal', 'footer']))
 		}
 	};
 }
