fe4025165f77ea888b581df5e58364bb80402621
Vladislav Polishchuk <vlad@polisuk.ru>
2023-02-25 20:56:21 +0300

    fix tests

--- __tests__/components/AppInputCode.test.tsx
+++ __tests__/components/AppInputCode.test.tsx
@@ -5,31 +5,64 @@ import AppInputCode from '../../components/AppInputCode/AppInputCode';
 
 describe('AppInputCode', () => {
 	it('should render', () => {
-		const wrapper = render(<AppInputCode codeLength={6} />);
+		const fn = jest.fn();
+
+		const wrapper = render(
+			<AppInputCode
+				submit={fn}
+				codeLength={6}
+			/>
+		);
+
 		const input = screen.getAllByTestId('app-inputCode')[0];
 		expect(input).toBeInTheDocument();
 		expect(wrapper).toMatchSnapshot();
 	});
 
 	it('should not have value', () => {
-		render(<AppInputCode codeLength={6} />);
+		const fn = jest.fn();
+
+		render(
+			<AppInputCode
+				submit={fn}
+				codeLength={6}
+			/>
+		);
+
 		const inputs = screen.getAllByTestId('app-inputCode');
 		inputs.forEach(input => expect(input).toHaveValue(null));
 	});
 
 	it('should have value', () => {
-		render(<AppInputCode codeLength={6} />);
+		const fn = jest.fn();
+
+		render(
+			<AppInputCode
+				submit={fn}
+				codeLength={6}
+			/>
+		);
+
 		const inputs = screen.getAllByTestId('app-inputCode');
 		inputs.forEach(input => {
 			fireEvent.input(input, { target: { value: 1 } });
 		});
 		inputs.forEach(input => expect(input).toHaveValue(1));
+		expect(fn).toBeCalled();
 	});
 
 	it('should have value with paste', () => {
-		render(<AppInputCode codeLength={6} />);
+		const fn = jest.fn();
+
+		render(
+			<AppInputCode
+				submit={fn}
+				codeLength={6}
+			/>
+		);
 		const inputs = screen.getAllByTestId('app-inputCode');
 		fireEvent.paste(inputs[0], { clipboardData: { getData: () => '123456' } });
 		inputs.forEach((input, index) => expect(input).toHaveValue(index + 1));
+		expect(fn).toBeCalled();
 	});
 });
