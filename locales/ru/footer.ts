69871ee8649bd772628afc356a91751ef1cc5359
Vladislav Polishchuk <vlad@polisuk.ru>
2023-04-16 22:12:41 +0300

    Change github username

--- locales/ru/footer.ts
+++ locales/ru/footer.ts
@@ -2,11 +2,11 @@ const footer = {
 	locale: 'ru',
 	title: "ДООМ.РУ — база данных о недвижимости. Использование сайта означает согласие с <a class='link_hover--underline' href='http://doom-estate.ru/ru/legal/user-agreement' rel='noopener noreferrer'>Пользовательским соглашением</a> и <a class='link_hover--underline' href='http://doom-estate.ru/ru/legal/privacy-policy' rel='noopener noreferrer'>Политикой конфиденциальности</a>. Оплачивая лицензионный платеж, вы принимаете <a class='link_hover--underline' href='http://doom-estate.ru/ru/legal/license-agreement' rel='noopener noreferrer'>Лицензионное соглашение.</a>",
 	disclaimer:
-		"Сайт не является коммерческим и создан в личных интересах <a class='link_hover--underline' href='https://github.com/vladislav124352' rel='noopener noreferrer'>автора</a>",
+		"Сайт не является коммерческим и создан в личных интересах <a class='link_hover--underline' href='https://github.com/vladpolisuk' rel='noopener noreferrer'>автора</a>",
 	links: {
 		github: {
 			title: 'Перейти в GitHub',
-			url: 'https://github.com/vladislav124352/doom-ru-client'
+			url: 'https://github.com/vladpolisuk/doom-ru-client'
 		}
 	}
 };
