5b37c079d9edfa4b7d42c0a0d2c2750fcd8f873c
Vladislav Polishchuk <vlad@polisuk.ru>
2023-04-18 01:32:16 +0300

    fixes in next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ const nextConfig = withPWA({
 	swcMinify: true,
 	env: {
 		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-		NEXT_PUBLIC_LOCATION_API_URL: process.env.NEXT_PUBLIC_LOCATION_API_URL
+		NEXT_PUBLIC_LOCATION_API_URL: process.env.NEXT_PUBLIC_LOCATION_API_URL,
+		NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST
 	},
 	images: {
 		domains: ['lh3.googleusercontent.com', 'localhost', 'api.doom-estate.ru', 'doom-estate.ru']
